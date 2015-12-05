c6420cba4fff8403995170df65c42ff653ea535c
CrazyYbf <774721759@qq.com>
2015-12-05 21:56:58 +0800

    Initial Commit

---
+++ ShSlApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShSlApp
+//
+//  Created by 易 彬锋 on 15/12/5.
+//  Copyright © 2015年 bfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
